b9035bcf8f06d35f635a28af1f23aca19866161d
rlierman <54672714+rlierman@users.noreply.github.com>
2019-08-29 08:34:39 -0500

    Create hello.c
    Added a line to print my major

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: RJ Lierman and John Tencer
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("RJ Lierman and John Tencer\n");
+
+  return 0;
+}
